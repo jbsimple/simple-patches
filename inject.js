64c617dbb8115e442d394199cd1041c90aa436c1
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-06-21 11:39:29 -0400

    URL patch
    More url patches

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 // use this with browser extension
 
 let script = document.createElement('script');
-script.src = "https://raw.githubusercontent.com/jbsimple/simple-patches/main/recom-patches.js";
+script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches.js?v=" + Date.now();
 Date.now();
 document.head.appendChild(script);
