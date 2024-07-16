37a39983ba55870f8a0c1232612b538439287e81
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-07-16 12:42:31 -0400

    Script Loading Listener
    Check to make sure scripts load

--- recom-patches.js
+++ recom-patches.js
@@ -1,4 +1,3 @@
-console.log('ReCom Patches Loaded');
 const nav_sidebar = document.getElementById('kt_app_sidebar_navs_wrappers');
 if (nav_sidebar) {
     nav_sidebar.style.display = 'flex';
@@ -18,35 +17,46 @@ if (nav_sidebar) {
 
 document.head.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches.css?v=' + Date.now() + '" type="text/css"/>';
 let script = document.createElement('script');
+script.name = 'n/a';
+script.onload = function() {
+    console.log('Patch Loaded:', script.name);
+};
 
 if (window.location.href.includes('/receiving/queues/listing/')) {
 
     script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches-listing.js?v=" + Date.now();
+    script.name = 'recom-patches-listing.js';
 
 }
 
 if (window.location.href.includes('/queues/conditions/')) {
     
     script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches-condqueue.js?v=" + Date.now();
-    
+    script.name = 'recom-patches-condqueue.js';
+
 }
 
 if (window.location.href.includes('/products') || window.location.href.includes('/product/item')) {
 
     document.head.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches-product.css?v=' + Date.now() + '" type="text/css"/>';
     script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches-productPage.js?v=" + Date.now();
+    script.name = 'recom-patches-productPage.js';
 
 }
 
 if (window.location.href.includes('/receiving') && document.getElementById('searchProductForm')) {
     
     script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches-newInventory.js?v=" + Date.now();
+    script.name = 'recom-patches-newInventory.js';
 
 }
 
 if (window.location.href.includes('/reports')) {
 
     script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches-reports.js?v=" + Date.now();
+    script.name = "recom-patches-reports.js";
+
 }
 
 document.body.appendChild(script);
+console.log('Patch Loading Complete');
