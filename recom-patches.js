546f794d840cd5091c67e59f2b48993d5a036cdd
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-07-16 11:55:18 -0400

    test 2
    testing more styling stuff

--- recom-patches.js
+++ recom-patches.js
@@ -3,7 +3,11 @@ const nav_sidebar = document.getElementById('kt_app_sidebar_navs_wrappers');
 if (nav_sidebar) {
     nav_sidebar.style.display = 'flex';
     nav_sidebar.style.flexDirection = 'column';
-    
+    nav_sidebar.style.height = '100%';
+
+    const nav_sidebar_links = document.getElementById('#kt_app_sidebar_menu');
+    nav_sidebar.style.flex = '1';
+
     const loaded_message = document.createElement('span');
     loaded_message.setAttribute('style', 'text-align: center;');
     loaded_message.textContent = 'Patches Loaded: 7-16-2024__3';
