a0971dcfa17adb59941c88a77e49a8d387a5177f
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-07-16 11:50:45 -0400

    testing new inject
    testing a new ay to embed code

--- recom-patches.js
+++ recom-patches.js
@@ -1,6 +1,9 @@
 console.log('ReCom Patches Loaded');
 const nav_sidebar = document.getElementById('kt_app_sidebar_navs_wrappers');
 if (nav_sidebar) {
+    nav_sidebar.style.display = 'flex';
+    nav_sidebar.style.flexDirection = 'column';
+    
     const loaded_message = document.createElement('span');
     loaded_message.setAttribute('style', 'text-align: center;');
     loaded_message.textContent = 'Patches Loaded: 7-16-2024__3';
