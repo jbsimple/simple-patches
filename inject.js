be0777ca3a7143fd1a9120656659f488295d4067
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-07-16 08:58:32 -0400

    fix inject.js
    I added a random date.now line for some reason

--- inject.js
+++ inject.js
@@ -2,5 +2,4 @@
 
 let script = document.createElement('script');
 script.src = "https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches.js?v=" + Date.now();
-Date.now();
 document.head.appendChild(script);
