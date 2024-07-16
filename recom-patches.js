cca6e812ca31d82276f9b3773e83ecc16cb324f2
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-07-16 09:34:43 -0400

    Added sidebar indicator
    Shows that the stuff has been loaded.

--- recom-patches.js
+++ recom-patches.js
@@ -1,4 +1,13 @@
 console.log('ReCom Patches Loaded');
+const nav_sidebar = document.getElementById('kt_app_sidebar_navs_wrappers');
+if (nav_sidebar) {
+    const loaded_message = document.createElement('span');
+    loaded_message.setAttribute('style', 'text-align: center;');
+    loaded_message.textContent = 'Patches Loaded: 7-16-2024__3';
+    nav_sidebar.appendChild(loaded_message);
+} else {
+    console.error('Sidebar could not be found.');
+}
 document.head.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jbsimple/simple-patches@main/recom-patches.css?v=' + Date.now() + '" type="text/css"/>';
 
 if (window.location.href.includes('/receiving/queues/listing/')) {
