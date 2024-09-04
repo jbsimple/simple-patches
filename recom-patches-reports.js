d5f812dbf93746070a62a593eaae3fc15f1d7299
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-09-04 09:37:03 -0400

    New report added

--- recom-patches-reports.js
+++ recom-patches-reports.js
@@ -325,6 +325,38 @@ function report_preset(name) {
         content.appendChild(userInputSubtext);
         
         content.appendChild(submit_button);
+    } else if (name === 'product_highQty') {
+        const submit_button = document.createElement('button');
+        submit_button.classList.add('btn');
+        submit_button.classList.add('btn-large');
+        submit_button.classList.add('btn-primary');
+        submit_button.setAttribute('onclick', `report_productHighQty();`);
+        submit_button.innerHTML = `Create
+            <span class="svg-icon svg-icon-4 ms-1 me-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor"></rect>
+            <path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor"></path>
+            </svg>
+            </span>`;
+        
+        const userInputSubtext = document.createElement('div');
+        userInputSubtext.setAttribute('style', 'flex: 1; display: flex; align-items: center;');
+        userInputSubtext.innerHTML = "Generate a report of high quantity items.<br>Includes in stock 50+ qty items. Columns for ASIN and dates included.";
+        
+        const userInputTitle = document.createElement('h4');
+        userInputTitle.classList.add('fw-bolder');
+        userInputTitle.classList.add('d-flex');
+        userInputTitle.classList.add('align-items-center');
+        userInputTitle.classList.add('text-dark');
+        userInputTitle.setAttribute('style', 'width: 200px;');
+        userInputTitle.textContent = 'High Qty Items:';
+
+        
+        content.appendChild(userInputTitle);
+        
+        content.appendChild(userInputSubtext);
+        
+        content.appendChild(submit_button);
     }
 
     return content;
@@ -488,6 +520,44 @@ function report_pictureMissingSpecial_submit() {
     getReport(request);
 }
 
+function report_productHighQty() {
+    const csrfToken = document.querySelector('input[name="csrf_recom"]').value;
+    
+    var request = {
+        report: {
+            type: "active_inventory",
+            columns: [
+                "products.sid",
+                "products.name",
+                "product_items.sku",
+                "product_items.condition_id",
+                "product_items.available",
+                "product_items.in_stock",
+                "product_items.price",
+                "product_items.location",
+                "products.brand_id",
+                "products.category_id",
+                "products.asin",
+                "product_items.is_scrap",
+                "product_items.has_fba",
+                "product_items.sold_at",
+                "product_items.priced_at",
+                "product_items.created_at",
+                "product_items.updated_at"
+            ],
+            filters: [
+                {
+                    column: "product_items.in_stock",
+                    opr: "{0} >= {1}",
+                    value: 50
+                }
+            ]
+        },
+        csrf_recom: csrfToken
+    };
+    
+    getReport(request);
+}
 
 function getReport(request) {
     console.log(request);
