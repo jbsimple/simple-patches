ddb85cb6f21910d4c97f545499aaaa65a441e3a7
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-10-22 11:24:46 -0400

    fix for activity log
    makes it prettier

--- recom-patches-productPage.js
+++ recom-patches-productPage.js
@@ -1,53 +1,35 @@
-// one day I will add this
-/*
-var media_tab = document.getElementById('rc_product_media_tab');
-var media_tree = document.getElementById('product-images-container');
-var media_tree_parent = media_tree.parentNode;
+function getMetaName(meta_id) {
+    const meta = [
+        { meta_id: 7, meta_name: "asin_check" },
+        { meta_id: 1, meta_name: "renewed_asin" },
+        { meta_id: 10, meta_name: "backmarket_check" },
+        { meta_id: 2, meta_name: "backmarket_id" },
+        { meta_id: 13, meta_name: "cycle_count" },
+        { meta_id: 12, meta_name: "marketplace_check" },
+        { meta_id: 11, meta_name: "oversold" },
+        { meta_id: 17, meta_name: "reebelo_id" },
+        { meta_id: 18, meta_name: "reebelo_id_-_canada" },
+        { meta_id: 16, meta_name: "renewed_premium_asin" },
+        { meta_id: 3, meta_name: "secondary_asins" },
+        { meta_id: 20, meta_name: "secondary_backmarket_ids" },
+        { meta_id: 5, meta_name: "secondary_gtin" },
+        { meta_id: 14, meta_name: "send_to_fba_-_yesno" },
+        { meta_id: 8, meta_name: "short_title" },
+        { meta_id: 9, meta_name: "walmart_check" },
+        { meta_id: 4, meta_name: "walmart_upc" },
+        { meta_id: 15, meta_name: "activity_log" }
+    ];
+    const metaItem = meta.find(item => item.meta_id === meta_id);
+    return metaItem ? metaItem.meta_name : null;
+  }
 
-if (media_tab && media_tree) {
-    var newElement = document.createElement('div');
-    newElement.classList.add('fv-row');
-    newElement.classList.add('mb-2');
-    newElement.style.paddingBottom = "1rem";
-
-    var button = document.createElement('button');
-    button.classList.add('btn');
-    button.classList.add('btn-danger');
-    button.textContent = 'Remove Images';
-    button.style.color = 'white';
-    button.style.border = 'none';
-    button.style.padding = '10px 20px';
-    button.style.cursor = 'pointer';
-    button.style.borderRadius = '5px';
-
-    button.addEventListener('click', function() {
-            var deleteImages = document.querySelectorAll('.delete-image');
-            deleteImages.forEach(function(item) {
-                    var id = item.getAttribute('data-id');
-                    var type = item.getAttribute('data-type');
-                    var remove = false;
-                    fetch("ajax/actions/productimagedelete/" + id, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "X-CSRF-TOKEN": document.querySelector('meta[name="X-CSRF-TOKEN"]').getAttribute("content")
-                        },
-                        body: JSON.stringify({ id: id, type: type })
-                        })
-                        .then(response => response.json())
-                        .then(data => {
-                        apiResponseAlert(data);
-                        remove = true;
-                        })
-                        .catch(error => {
-                        console.log("FAIL", error);
-                        });
-                        if (remove) {
-                            item.remove();
-                        }
-                });
-    });
-
-    newElement.appendChild(button);
-    media_tree_parent.insertBefore(newElement, media_tree);
-} */
+document.querySelectorAll('.json__key').forEach(function(keyDiv) {
+    if (keyDiv.textContent.trim() === 'meta_id') {
+        let nextDiv = keyDiv.nextElementSibling;
+        if (nextDiv && nextDiv.classList.contains('json__value') && nextDiv.classList.contains('json__value--update')) {
+            const meta_id = parseInt(nextDiv.textContent, 10);
+            const meta_name = getMetaName(meta_id);
+            nextDiv.textContent = `(${meta_id}) ${meta_name}`;
+        }
+    }
+});
--- recom-patches.js
+++ recom-patches.js
@@ -1,4 +1,4 @@
-const version = '10-03-2024__1';
+const version = '10-22-2024__1';
 
 const nav_sidebar = document.getElementById('kt_app_sidebar_navs_wrappers');
 if (nav_sidebar) {
