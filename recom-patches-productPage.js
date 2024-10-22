81bd06fd4a3f9ccd862b5a933cbbda3eb8474a34
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-10-22 11:40:56 -0400

    Update recom-patches-productPage.js

--- recom-patches-productPage.js
+++ recom-patches-productPage.js
@@ -23,14 +23,15 @@ function getMetaName(meta_id) {
     return metaItem ? metaItem.meta_name : null;
 }
 
-document.querySelectorAll('.json__key').forEach(function(keyDiv) {
-    if (keyDiv.textContent.trim() === 'meta_id') {
-        console.error(keyDiv.textContent.trim());  
-        let nextDiv = keyDiv.nextElementSibling;
-        if (nextDiv) {
-            const meta_id = parseInt(nextDiv.textContent, 10);
-            const meta_name = getMetaName(meta_id);
-            nextDiv.textContent = `(${meta_id}) ${meta_name}`;
+setTimeout(function() {
+    document.querySelectorAll('.json__key').forEach(function(keyDiv) {
+        if (keyDiv.textContent.trim() === 'meta_id') {
+            let nextDiv = keyDiv.nextElementSibling;
+            if (nextDiv && nextDiv.classList.contains('json__value') && nextDiv.classList.contains('json__value--update')) {
+                const meta_id = parseInt(nextDiv.textContent, 10);
+                const meta_name = getMetaName(meta_id);
+                nextDiv.textContent = `(${meta_id}) ${meta_name}`;
+            }
         }
-    }
-});
+    });
+}, 500);
