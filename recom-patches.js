bf86dc34b97e6882ff26538081f2c85b2fbf57c4
jbsimple <jonathan.bitterli@simplecellinc.com>
2024-12-31 13:16:55 -0500

    test7

--- recom-patches.js
+++ recom-patches.js
@@ -97,7 +97,7 @@ function modifiedClockInit() {
 }
 
 function modifiedClock(task) {
-    const modal = `<div class="modal fade show" id="patch_clockout_fullModal" data-bs-backdrop="static" tabindex="-1" aria-modal="true" role="dialog" style="display: block; background: rgba(0, 0, 0, .4) !important;">
+    const modal = `<div class="modal fade" id="patch_clockout_fullModal" data-bs-backdrop="static" tabindex="-1" aria-modal="true" role="dialog" style="background: rgba(0, 0, 0, .4) !important;">
         <div class="modal-dialog modal-dialog-centered mw-650px">
             <div class="modal-content rounded">
                 <div class="modal-header">
@@ -192,6 +192,12 @@ function modifiedClock(task) {
                     fullModal.remove();
                 }
             };
+        }
+
+        const newModal = document.getElementById('patch_clockout_fullModal');
+        if (newModal) {
+            newModal.style.display = 'block';
+            newModal.classList.add('show');
         }
 
     } else {
