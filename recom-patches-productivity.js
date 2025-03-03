e767c14b92dd8851f24e36f21e80e9dc7ca3a035
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-03-03 10:48:34 -0500

    COLOR

--- recom-patches-productivity.js
+++ recom-patches-productivity.js
@@ -482,7 +482,7 @@ function injectDateSelect(funct, content) {
     dateInput.type = "date";
     dateInput.id = "patches-productivity-dateInput";
     dateInput.classList.add('form-control', 'rounded-1');
-    dateInput.setAttribute('style', 'color: var(--bs-light-inverse); width: unset;');
+    dateInput.setAttribute('style', 'color:var(--bs-text-gray-800); width: unset;');
 
     const today = new Date().toISOString().split('T')[0];
     dateInput.value = today;
