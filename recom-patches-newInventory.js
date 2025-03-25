1e8fd4faec9db160a801fc810b595b0aa5093fd2
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-03-25 08:41:51 -0400

    Update recom-patches-newInventory.js
    test0

--- recom-patches-newInventory.js
+++ recom-patches-newInventory.js
@@ -81,7 +81,6 @@ if (inventory_results) {
                 const target = nextElement.getAttribute('target');
                 if (href && target === '_blank') {
                     const newLink = document.createElement('a');
-                    newLink.href = "#";
                     newLink.textContent = span.textContent;
                     newLink.classList.add('text-success', 'fw-bold', 'ajax-modal');
                     newLink.setAttribute('data-url', `ajax/modals/productitems/${href.split("/").pop()}`);
