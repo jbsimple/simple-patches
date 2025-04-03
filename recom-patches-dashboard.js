22b8712d9b1c6c26226e4e36663a1f7d0bd2cb68
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-03 07:03:32 -0400

    Update recom-patches-dashboard.js
    test0.2

--- recom-patches-dashboard.js
+++ recom-patches-dashboard.js
@@ -1,27 +1,31 @@
-const content_container = document.getElementById('kt_app_content_container');
+function initQuickLinks() {
+    const content_container = document.getElementById('kt_app_content_container');
 
-const quickLinks = document.createElement('div');
-quickLinks.id = 'patches-dashboard-quicklinks';
+    const quickLinks = document.createElement('div');
+    quickLinks.id = 'patches-dashboard-quicklinks';
 
-const quicklinks = [
-    {"label":"Your Productivity", "href":"/productivity/employee/0"},
-    {"label":"Team Productivity", "href":"/productivity"},
-    {"label":"Items Catalog", "href":"/product/items"},
-    {"label":"Products Catalog", "href":"/products"},
-    {"label":"New Inventory", "href":"/receiving"},
-    {"label":"Pending Listing", "href":"/receiving/queues/listing"},
-    {"label":"FBA Check", "href":"/receiving/queues/fba-check"},
-    {"label":"Pending Inventory", "href":"/receiving/queues/inventory"}
-]
+    const quicklinks = [
+        {"label":"Your Productivity", "href":"/productivity/employee/0"},
+        {"label":"Team Productivity", "href":"/productivity"},
+        {"label":"Items Catalog", "href":"/product/items"},
+        {"label":"Products Catalog", "href":"/products"},
+        {"label":"New Inventory", "href":"/receiving"},
+        {"label":"Pending Listing", "href":"/receiving/queues/listing"},
+        {"label":"FBA Check", "href":"/receiving/queues/fba-check"},
+        {"label":"Pending Inventory", "href":"/receiving/queues/inventory"}
+    ]
 
-quicklinks.forEach(link => function() {
-    const quickLink = document.createElement('a');
-    quickLink.setAttribute('class', 'card card-flush h-md-50 mb-xl-10 blockui');
-    quickLink.setAttribute('href', link.href);
-    quickLink.innerHTML = `<div class="card-header pt-5">
-        <h3 class="card-title text-gray-800">${item.label}</h3>
-    </div>`;
-    quickLinks.appendChild(quickLink);
-});
+    quicklinks.forEach(link => function() {
+        const quickLink = document.createElement('a');
+        quickLink.setAttribute('class', 'card card-flush h-md-50 mb-xl-10 blockui');
+        quickLink.setAttribute('href', link.href);
+        quickLink.innerHTML = `<div class="card-header pt-5">
+            <h3 class="card-title text-gray-800">${item.label}</h3>
+        </div>`;
+        quickLinks.appendChild(quickLink);
+    });
 
-content_container.prepend(quickLinks);
+    content_container.prepend(quickLinks);
+}
+
+setTimeout(function () { initQuickLinks(); }, 200);
