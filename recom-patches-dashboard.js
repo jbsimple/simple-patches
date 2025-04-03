4cdd4407b7c5ec84bab5406e5b70fd1207dd5e40
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-03 07:47:19 -0400

    Update recom-patches-dashboard.js

--- recom-patches-dashboard.js
+++ recom-patches-dashboard.js
@@ -83,7 +83,7 @@ function initQuickLinks() {
 
     const quickLinksLabel = document.createElement('span');
     quickLinksLabel.setAttribute('class', 'text-dark fw-bolder fs-3');
-    quickLinksLabel.setAttribute('margin: 1rem; margin-top: 0;');
+    quickLinksLabel.setAttribute('style', 'margin: 1rem; margin-top: 0;');
     quickLinksLabel.textContent = "Quick Links:";
     content_container.prepend(quickLinksLabel);
 }
