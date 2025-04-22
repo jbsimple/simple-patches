6be203d9968630d8c0279e4bc49eb77f31837c37
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-22 11:42:32 -0400

    Update recom-patches-listing.js

--- recom-patches-listing.js
+++ recom-patches-listing.js
@@ -41,7 +41,7 @@ function fixSimilarProduct() {
     });
 }
 
-async function getTimeSpentInMinutes() {
+async function getTimeSpentInMinutes(sku) {
     async function getUserID() {
         try {
             const response = await fetch('/user/me');
@@ -77,7 +77,6 @@ async function getTimeSpentInMinutes() {
 
         let date = todayFormatted;
         const userId = await getUserID();
-        const sku = 'wwwN52619-51'; //test
 
         let request = {
             report: {
@@ -146,114 +145,6 @@ function isValidBarcode(value) {
     }
   
     return ((10 - (result % 10)) % 10) === parseInt(paddedValue.charAt(13), 10);
-}
-
-if (gtin_input) {
-    initGTIN = gtin_input.value;
-    curGTIN = gtin_input.value;
-
-    if (gtin_input.value || gtin_input.getAttribute('value')) {
-        verifyGTIN();
-    }
-    
-    gtin_input.addEventListener('input', function() {
-        verifyGTIN();
-    });
-    
-    if (generateButton) {
-            generateButton.addEventListener('click', function() {
-
-                    setTimeout(function() {
-                            verifyGTIN();
-                    }, 500); // yikes
-            });
-    }
-    
-    function verifyGTIN() {
-        // update flag
-        curGTIN = gtin_input.value;
-
-        var valueLength = gtin_input.value.length;
-        console.log(valueLength);
-        
-        if (valueLength > 12 || !isValidBarcode(curGTIN)) {
-            gtin_input.style.outline = "2px solid var(--bs-danger)";
-            gtin_input.style.backgroundColor = "color-mix(in srgb, var(--bs-danger) 15%, rgb(255,255,255,0))";
-            addInvalidFeedback();
-        } else {
-            gtin_input.style.outline = "";
-            gtin_input.style.backgroundColor = "";
-            removeInvalidFeedback();
-        }
-    }
-    
-    function addInvalidFeedback() {
-        if (!document.getElementById('gtin-feedback')) {
-            var feedbackDiv = document.createElement('div');
-            feedbackDiv.id = 'gtin-feedback';
-            feedbackDiv.className = 'fv-plugins-message-container invalid-feedback';
-            feedbackDiv.textContent = 'The GTIN is invalid';
-
-            gtin_input.parentNode.appendChild(feedbackDiv);
-        }
-    }
-    
-    function removeInvalidFeedback() {
-        var feedbackDiv = document.getElementById('gtin-feedback');
-        if (feedbackDiv) {
-            feedbackDiv.parentNode.removeChild(feedbackDiv);
-        }
-    }
-
-    if (listingSubmit && listingResults) {
-        listingSubmit.addEventListener('click', function() {
-            setTimeout(function() {
-                if (initGTIN !== curGTIN) {
-                    var code = `<br>
-                    <strong class="patches-warning">
-                        <i class="fa fa-triangle-exclamation fs-2"></i>
-                        <span>GTIN Change Detected!</span>
-                        <i class="fa fa-triangle-exclamation fs-2"></i>
-                    </strong>
-                    <br>
-                    <div class="patches-column">
-                        <div class="patches-row">
-                            <strong>Original Queue GTIN:</strong>
-                            <input type="text" class="form-control form-control-solid form-control-lg" disabled value="${initGTIN}"></input>
-                        </div>
-                        <div class="patches-row">
-                            <strong>Created Listing GTIN:</strong>
-                            <input type="text" class="form-control form-control-solid form-control-lg" disabled value="${curGTIN}"></input>
-                        </div>
-                    </div>
-                    <div class="patches-column">
-                        <span>With the GTIN change detected, you can change it back here. The fields below will update the product.</span>
-                    </div>
-                    <div class="patches-column">
-                        <label for="patches-oldgtin">Product GTIN:</label>
-                        <input type="text" id="patches-oldgtin" class="form-control form-control-solid form-control-lg" value="${initGTIN}"></input>
-                    </div>
-                    <div class="patches-column">
-                        <label for="patches-newgtin">Product Secondary GTIN:</label>
-                        <input type="text" id="patches-newgtin" class="form-control form-control-solid form-control-lg" value="${curGTIN}"></input>
-                    </div>
-                    <div class="patches-column">
-                        <span style="flex: 1;">You can set the GTIN back to the original and the generated GTIN as the secondary by pressing the button below.<br><br>
-                            * Old GTIN becomes the product's real GTIN.<br>
-                            * Current GTIN becomes the product's secondary GTIN.
-                            * If it doesn't save here, the GTIN is REALLY invalid and there's nothing that can be done.
-                        </span>
-                        <strong>In order for the switch to happen, you must hit the 'Update GTINS' button! It is NOT Automatic!</strong>
-                    </div>
-                    <div class="patches-row">
-                        <a class="btn btn-lg btn-light-warning me-3" onclick="productGTIN()">Update GTINS</a>
-                        <div style="flex: 1;" id="productsGTIN-response"></div>
-                    </div>`;
-                    listingResults.innerHTML += code;
-                }
-            }, 500); // yikes
-        });
-    }
 }
 
 function productGTIN() {
@@ -376,6 +267,123 @@ const observer = new MutationObserver(() => {
         }
     });
 });
+
+async function initListingPatch() {
+    if (gtin_input) {
+        initGTIN = gtin_input.value;
+        curGTIN = gtin_input.value;
+    
+        if (gtin_input.value || gtin_input.getAttribute('value')) {
+            verifyGTIN();
+        }
+        
+        gtin_input.addEventListener('input', function() {
+            verifyGTIN();
+        });
+        
+        if (generateButton) {
+            generateButton.addEventListener('click', function() {
+                setTimeout(function() { verifyGTIN(); }, 500); // yikes
+            });
+        }
+        
+        function verifyGTIN() {
+            // update flag
+            curGTIN = gtin_input.value;
+    
+            var valueLength = gtin_input.value.length;
+            console.log(valueLength);
+            
+            if (valueLength > 12 || !isValidBarcode(curGTIN)) {
+                gtin_input.style.outline = "2px solid var(--bs-danger)";
+                gtin_input.style.backgroundColor = "color-mix(in srgb, var(--bs-danger) 15%, rgb(255,255,255,0))";
+                addInvalidFeedback();
+            } else {
+                gtin_input.style.outline = "";
+                gtin_input.style.backgroundColor = "";
+                removeInvalidFeedback();
+            }
+        }
+        
+        function addInvalidFeedback() {
+            if (!document.getElementById('gtin-feedback')) {
+                var feedbackDiv = document.createElement('div');
+                feedbackDiv.id = 'gtin-feedback';
+                feedbackDiv.className = 'fv-plugins-message-container invalid-feedback';
+                feedbackDiv.textContent = 'The GTIN is invalid';
+    
+                gtin_input.parentNode.appendChild(feedbackDiv);
+            }
+        }
+        
+        function removeInvalidFeedback() {
+            var feedbackDiv = document.getElementById('gtin-feedback');
+            if (feedbackDiv) {
+                feedbackDiv.parentNode.removeChild(feedbackDiv);
+            }
+        }
+    
+        if (listingSubmit && listingResults) {
+            listingSubmit.addEventListener('click', function() {
+                setTimeout(async function() {
+                    if (listingResults) {
+                        const getCreatedSKU = await listingResults.querySelectorAll('h2');
+                        if (getCreatedSKU && getCreatedSKU[0]) {
+                            const timespentinminutes = getTimeSpentInMinutes(getCreatedSKU[0].textContent);
+                            listingResults.innerHTML += `<br><p><b>Time Spent in Minutes:</b><br>${timespentinminutes} minutes.</p>`;
+                        }
+                    }
+    
+                    if (initGTIN !== curGTIN) {
+                        var code = `<br>
+                        <strong class="patches-warning">
+                            <i class="fa fa-triangle-exclamation fs-2"></i>
+                            <span>GTIN Change Detected!</span>
+                            <i class="fa fa-triangle-exclamation fs-2"></i>
+                        </strong>
+                        <br>
+                        <div class="patches-column">
+                            <div class="patches-row">
+                                <strong>Original Queue GTIN:</strong>
+                                <input type="text" class="form-control form-control-solid form-control-lg" disabled value="${initGTIN}"></input>
+                            </div>
+                            <div class="patches-row">
+                                <strong>Created Listing GTIN:</strong>
+                                <input type="text" class="form-control form-control-solid form-control-lg" disabled value="${curGTIN}"></input>
+                            </div>
+                        </div>
+                        <div class="patches-column">
+                            <span>With the GTIN change detected, you can change it back here. The fields below will update the product.</span>
+                        </div>
+                        <div class="patches-column">
+                            <label for="patches-oldgtin">Product GTIN:</label>
+                            <input type="text" id="patches-oldgtin" class="form-control form-control-solid form-control-lg" value="${initGTIN}"></input>
+                        </div>
+                        <div class="patches-column">
+                            <label for="patches-newgtin">Product Secondary GTIN:</label>
+                            <input type="text" id="patches-newgtin" class="form-control form-control-solid form-control-lg" value="${curGTIN}"></input>
+                        </div>
+                        <div class="patches-column">
+                            <span style="flex: 1;">You can set the GTIN back to the original and the generated GTIN as the secondary by pressing the button below.<br><br>
+                                * Old GTIN becomes the product's real GTIN.<br>
+                                * Current GTIN becomes the product's secondary GTIN.
+                                * If it doesn't save here, the GTIN is REALLY invalid and there's nothing that can be done.
+                            </span>
+                            <strong>In order for the switch to happen, you must hit the 'Update GTINS' button! It is NOT Automatic!</strong>
+                        </div>
+                        <div class="patches-row">
+                            <a class="btn btn-lg btn-light-warning me-3" onclick="productGTIN()">Update GTINS</a>
+                            <div style="flex: 1;" id="productsGTIN-response"></div>
+                        </div>`;
+                        listingResults.innerHTML += code;
+                    }
+                }, 500); // yikes
+            });
+        }
+    }
+}
+
+initListingPatch();
 
 observer.observe(document.body, {
     childList: true,
