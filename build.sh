7c9f5526c89c90dcd0acd94299a42d42069b9efa
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-29 13:27:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# build.sh
 
 echo "Generating buildInfo.json..."
 
+short_sha=$(echo "${VERCEL_GIT_COMMIT_SHA}" | cut -c1-7)
+
 cat <<EOF > buildInfo.json
 {
-  "buildId": "${BUILD_ID}",
+  "buildId": "${short_sha}",
   "commitMessage": "${VERCEL_GIT_COMMIT_MESSAGE}",
   "repoBranch": "${VERCEL_GIT_COMMIT_REF}"
 }
 EOF
+
+echo "buildInfo.json generated:"
+cat buildInfo.json
