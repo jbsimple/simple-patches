4f8fcfad67cd293c3970f739545824aba4a4b418
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-29 11:48:00 -0400

    Update recom-patches-productPage.js
    idk

--- recom-patches-productPage.js
+++ recom-patches-productPage.js
@@ -1,13 +1,13 @@
 /* transcribe meta fields in activity log */
 
 /* fetch live data */
-let livemeta = [];
+let metakeys = [];
 async function fetchMeta() {
     const url = `/datatables/meta?draw=1&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=20&search%5Bvalue%5D=&search%5Bregex%5D=false&_=${Date.now()}`;
     const response = await fetch(url);
     const json = await response.json();
 
-    livemeta = json.data.map(row => {
+    metakeys = json.data.map(row => {
         const metaNameMatch = row[1].match(/<pre[^>]*>(.*?)<\/pre>/);
         const meta_name = metaNameMatch ? metaNameMatch[1] : null;
 
@@ -19,11 +19,11 @@ async function fetchMeta() {
 }
 
 async function getMetaName(meta_id) {
-    if (livemeta.length === 0) {
+    if (metakeys.length === 0) {
         await fetchMeta();
     }
 
-    const metaItem = livemeta.find(item => item.meta_id === meta_id);
+    const metaItem = metakeys.find(item => item.meta_id === meta_id);
     return metaItem ? metaItem.meta_name : null;
 }
 
