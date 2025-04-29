5e12aafc47e6e9d766c5bc7c47a1c6775ccb782b
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-29 13:46:33 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,13 @@ short_sha=$(echo "${VERCEL_GIT_COMMIT_SHA}" | cut -c1-7)
 cat <<EOF > buildInfo.js
 window.BUILD_INFO = {
     buildId: "${short_sha}",
+    fullCommitId: "${VERCEL_GIT_COMMIT_SHA}",
     commitMessage: "${VERCEL_GIT_COMMIT_MESSAGE}",
-    repoBranch: "${VERCEL_GIT_COMMIT_REF}"
+    repoBranch: "${VERCEL_GIT_COMMIT_REF}",
+    repoOwner: "${VERCEL_GIT_REPO_OWNER}",
+    repoName: "${VERCEL_GIT_REPO_SLUG}",
+    environment: "${VERCEL_ENV}",
+    deployUrl: "https://${VERCEL_URL}"
 };
 EOF
 
