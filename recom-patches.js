2becbe86e7e1f2caa5e10c67cfca5f5e21382385
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-04-29 09:41:49 -0400

    Update recom-patches.js
    sneaky

--- recom-patches.js
+++ recom-patches.js
@@ -33,11 +33,19 @@ if (nav_sidebar) {
 
     const name = nav_sidebar_links.querySelectorAll('.menu-heading')[0];
     currentuser = name.textContent.replace(/^Hi,\s*/, '').toLocaleLowerCase();
+    let icon = "https://media.tenor.com/1MG3j4q4W5AAAAAj/cat-jam.gif";
+
     if (name && name.textContent.includes('Hi, Luke')) {
+        icon = "https://i.redd.it/kfrhf84rmw3b1.jpg"; // abe
         name.textContent = 'Hi, Psychopath';
     } else if (name && name.textContent.includes('Hi, Nate')) {
         name.textContent = 'Hi, Nasty Nate';
     }
+
+    const avatars = document.querySelectorAll('img[src*="assets/media/svg/avatars/"]');
+    avatars.forEach(avatar => {
+        avatar.src = icon;
+    });
 
     const links = nav_sidebar_links.querySelectorAll('.menu-link');
     if (links.length > 0) {
