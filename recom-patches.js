d283018153a1e2ebb77f99b486731d5b3406ecc9
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-05-19 09:56:13 -0400

    Update recom-patches.js

--- recom-patches.js
+++ recom-patches.js
@@ -690,11 +690,14 @@ function hijackAjaxModal() {
                                     }
 
                                     if (sku !== '') {
+                                        const existingBadges = link.querySelectorAll('badge');
+                                        existingBadges.forEach(badge => {
+                                            badge.classList = 'badge badge-warning';
+                                        })
                                         const skuObj = image_counts.find((item) => item.sku.trim() === sku);
                                         if (skuObj.Item_Status === 'Inactive') {
                                             link.innerHTML += `<span class="badge badge-danger ms-2">Inactive</span>`;
                                         }
-
 
                                         const createdCell = document.createElement('td');
                                         if (skuObj && skuObj.Created_Date) {
