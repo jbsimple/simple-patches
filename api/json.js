c1a527ee77e038bcf38b638cccb1194587d0d8a1
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-08-05 07:19:03 -0400

    json cors

--- api/json.js
+++ api/json.js
@@ -1,6 +1,15 @@
 const { get } = require('@vercel/edge-config');
 
 module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     const key = Object.keys(req.query)[0];
 
     if (!key) {
