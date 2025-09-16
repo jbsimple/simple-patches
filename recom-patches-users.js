7f26e038d03de9af0360d3bd0e6862324db3e38a
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-09-16 10:59:42 -0400

    Update recom-patches-users.js

--- recom-patches-users.js
+++ recom-patches-users.js
@@ -1,4 +1,4 @@
-async function downloadUserLog(interval) {
+async function downloadUserLog(interval = 3) {
     // interval is how many days back to go
     let userID = null;
     const pathParts = window.location.pathname.split("/");
@@ -65,3 +65,37 @@ async function downloadUserLog(interval) {
         fireSwal("Error", "Could not fetch logs. Check console for details.", "error");
     }
 }
+
+function downloadUserLogsInit() {
+    const LogEntriesTable = document.getElementById('LogEntriesTable');
+    if (LogEntriesTable) {
+        const parent_card = LogEntriesTable.parentElement.parentElement;
+        if (parent_card.classList.contains('card')) {
+            const card_title = parent_card.querySelector('.card-header > .card-title');
+            if (card_title) {
+                const card_toolbar = document.createElement('div');
+                card_toolbar.classList = 'card-toolbar';
+
+                const downloadLogButton = document.createElement('button');
+                downloadLogButton.classList.add('btn', 'btn-info');
+                downloadLogButton.id = 'patches_downloadUserLogs';
+                downloadLogButton.textContent = 'Download';
+                downloadLogButton.title = "Downloads logs from past 3 days.";
+                downloadLogButton.style.cssText = `
+                    color: white;
+                    border: none;
+                    padding: 10px 20px;
+                    cursor: pointer;
+                    border-radius: 5px;
+                `;
+                downloadLogButton.onclick = () => downloadUserLog(3);
+                card_toolbar.appendChild(donloadLogButton);
+
+                card_title.insertAdjacentElement('afterend', card_toolbar);
+            }
+        }
+    }
+}
+
+// this is already in source html so this should be fine to run
+downloadUserLogsInit();
