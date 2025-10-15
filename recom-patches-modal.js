0705f64393ace24445d61c5d1a941fd6827278ff
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-10-15 10:31:49 -0400

    Update recom-patches-modal.js

--- recom-patches-modal.js
+++ recom-patches-modal.js
@@ -551,11 +551,15 @@ async function updatePictureLocations() {
 function patchesSettingsModal() {
     const body = `
         <style>
+            .patches-twowrap,
             .patches-threewrap {
                 display: flex;
                 flex-wrap: wrap;
                 gap: 1rem;
                 justify-content: space-between;
+            }
+            .patches-twowrap > div {
+                width: calc(50% - 1rem);
             }
             .patches-threewrap > div {
                 width: calc(33% - 1rem);
@@ -568,15 +572,17 @@ function patchesSettingsModal() {
             <p class="fs-6 fw-semibold form-label mb-2">If you want to have a list of commonly used activities for uniformity.</p>
             <textarea style="max-height: 20vh;" class="form-control form-control-solid" rows="2" name="activitylist" id="patch_setting_activitylist" placeholder="Enter comma-separated list here." spellcheck="false"></textarea>
         </div>
-        <div class="d-flex flex-column mb-8">
-            <label class="fs-6 fw-bold mb-2" for="patch_setting_pfpurl">Custom PFP URL:</label>
-            <p class="fs-6 fw-semibold form-label mb-2">If you want to add a custom profile picture for the top bar.</p>
-            <input type="text" class="form-control form-control-solid" name="pfpurl" id="patch_setting_pfpurl" placeholder="Enter URL" value="" spellcheck="false">
-        </div>
-        <div class="d-flex flex-column mb-8">
-            <label class="fs-6 fw-bold mb-2" for="patch_setting_bgurl">Custom Background Image URL:</label>
-            <p class="fs-6 fw-semibold form-label mb-2">If you want to add a custom background to each page.</p>
-            <input type="text" class="form-control form-control-solid" name="bgurl" id="patch_setting_bgurl" placeholder="Enter URL" value="" spellcheck="false">
+        <div class="d-flex mb-8 patches-twowrap">
+            <div class="d-flex flex-column mb-8">
+                <label class="fs-6 fw-bold mb-2" for="patch_setting_pfpurl">Custom PFP URL:</label>
+                <p class="fs-6 fw-semibold form-label mb-2">If you want to add a custom profile picture for the top bar.</p>
+                <input type="text" class="form-control form-control-solid" name="pfpurl" id="patch_setting_pfpurl" placeholder="Enter URL" value="" spellcheck="false">
+            </div>
+            <div class="d-flex flex-column mb-8">
+                <label class="fs-6 fw-bold mb-2" for="patch_setting_bgurl">Custom Background Image URL:</label>
+                <p class="fs-6 fw-semibold form-label mb-2">If you want to add a custom background to each page.</p>
+                <input type="text" class="form-control form-control-solid" name="bgurl" id="patch_setting_bgurl" placeholder="Enter URL" value="" spellcheck="false">
+            </div>
         </div>
         <div class="d-flex mb-8 patches-threewrap">
             <div class="d-flex flex-column mb-8">
@@ -638,7 +644,7 @@ function patchesSettingsModal() {
         title: 'Patches Settings',
         body,
         footer,
-        width: '800px',
+        width: '1000px',
         focus: null,
         escapeBlockedWhen: (ae) => ae
     });
