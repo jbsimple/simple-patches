2e7b90edce91792194e132a4fb986236783c1501
jbsimple <jonathan.bitterli@simplecellinc.com>
2025-12-02 11:32:44 -0500

    hiding productivity mockup setting
    this is just not being used right now

--- recom-patches-modal.js
+++ recom-patches-modal.js
@@ -590,7 +590,7 @@ function patchesSettingsModal() {
             <p class="fs-6 fw-semibold form-label mb-2">If you want to have a list of commonly used activities for uniformity.</p>
             <textarea style="max-height: 20vh;" class="form-control form-control-solid" rows="2" name="activitylist" id="patch_setting_activitylist" placeholder="Enter comma-separated list here." spellcheck="false"></textarea>
         </div>
-        <div class="d-flex mb-8 patches-twowrap">
+        <div class="d-flex mb-8 patches-twowrap" style="display: none !important;">
             <div class="d-flex flex-column mb-8">
                 <label class="fs-6 fw-bold mb-2" for="patch_setting_mockupProductivity">Enable Mockup Productivity:</label>
                 <p class="fs-6 fw-semibold form-label mb-2">This is the mockup I am using for the KMP.</p>
