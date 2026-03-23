364f186bb1cc1e69d142c1648f9b45919bcde47a
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-03-23 10:57:07 -0400

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -51,7 +51,13 @@ export default async function handler(req, res) {
         return res.status(400).json({ success: false, error: "Invalid route" });
     }
 
-    let apiURL = `https://${hostname}/api/v1/reports`;
+    let apiURL;
+
+    if (route === "meta") {
+        apiURL = `https://${hostname}/v1/reports/meta`;
+    } else if (route === "reports") {
+        apiURL = `https://${hostname}/api/v1/reports`;
+    }
 
     try {
         if (route === "meta") {
