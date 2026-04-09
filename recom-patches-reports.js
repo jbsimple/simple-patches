67baf89f7f764000b009df92f59fba36bce5bccf
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-04-09 14:51:00 -0400

    Update recom-patches-reports.js

--- recom-patches-reports.js
+++ recom-patches-reports.js
@@ -212,7 +212,7 @@ function initPreset() {
     const params = new URLSearchParams(window.location.search);
     let template = params.get('template') || '';
     if (template && template !== '') {
-        card_body.style.display = 'none';
+        card_body.innetHTML = '<p>Loading...</p>';
         const html = report_preset(template);
         if (html) {
             const inputCheck = html.querySelectorAll('input');
