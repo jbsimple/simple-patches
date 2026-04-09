dba72ada8f0d3f29062f2ca66368fb6fb8513979
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-04-09 13:36:12 -0400

    Update recom-patches-dashboard.js

--- recom-patches-dashboard.js
+++ recom-patches-dashboard.js
@@ -96,6 +96,7 @@ async function dashboardAlerts() {
     document.getElementById('productsStats').querySelector('.card-title').textContent = 'Stats';
 
     const container = document.getElementById('productsStats').querySelector('.card-body');
+    container.style.setProperty('min-height', '200px', 'important');
     container.querySelectorAll('div').forEach(elem => { elem.style.setProperty('display', 'none', 'important'); })
 
     await warning_photoRecent();
