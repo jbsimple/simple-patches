d447b460317be38890a81d95ac11eae81fde9138
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-06-16 07:27:29 -0400

    Update recom-patches-errors.js

--- recom-patches-errors.js
+++ recom-patches-errors.js
@@ -94,7 +94,7 @@ function initPrettyPrint() {
                     const mm = String(date.getMonth() + 1).padStart(2, '0');
                     const dd = String(date.getDate()).padStart(2, '0');
                     const yyyy = date.getFullYear();
-                    return `${mm}/${dd}/${yyyy}`;
+                    return `${yyyy}-${mm}-${dd}`
                 };
 
                 const range = `${formatDate(past)} - ${formatDate(today)}`;
