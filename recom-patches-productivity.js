f749437af48dffad0d2e3354d6aa8454d32e1b29
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-09-21 12:33:29 -0400

    Update recom-patches-productivity.js

--- recom-patches-productivity.js
+++ recom-patches-productivity.js
@@ -620,7 +620,7 @@ async function injectUserReport() {
                 const unitBox = `
                     <div class="card card-xl-stretch mb-xl-8" style="--bs-card-bg: rgb(65,40,50); color: white !important; flex: 1; min-width: 400px; margin-bottom: 0 !important;">
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex flex-column flex-grow-1" style="margin-bottom: 1.5rem;">
+                            <div class="d-flex flex-column flex-grow-1">
                                 <span class="text-white fw-bolder fs-3">${task.toLowerCase() === 'pictures' ? 'Images Uplodaded (Items)' : 'Units Added'} | ${label}</span>
                             </div>
                             <div class="pt-5">
@@ -634,7 +634,7 @@ async function injectUserReport() {
                 const timeBox = `
                     <div class="card card-xl-stretch mb-xl-8" style="--bs-card-bg: rgb(50,60,85) !important; color: white !important; flex: 1; min-width: 400px;">
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex flex-column flex-grow-1" style="margin-bottom: 1.5rem;">
+                            <div class="d-flex flex-column flex-grow-1">
                                 <span class="text-white fw-bolder fs-3">Time Spent | ${label}</span>
                             </div>
                             <div class="pt-5">
@@ -840,7 +840,7 @@ async function injectTeamReport() {
                     const unitBox = `
                         <div class="card card-xl-stretch mb-xl-8" style="--bs-card-bg: rgb(65,40,50); color: white !important; flex: 1; min-width: 400px; margin-bottom: 0 !important;">
                             <div class="card-body d-flex flex-column">
-                                <div class="d-flex flex-column flex-grow-1" style="margin-bottom: 1.5rem;">
+                                <div class="d-flex flex-column flex-grow-1">
                                     <span class="text-white fw-bolder fs-3">${task.toLowerCase() === 'pictures' ? 'Images Uplodaded (Items)' : 'Units Added'} | ${label}</span>
                                 </div>
                                 <div class="pt-5">
