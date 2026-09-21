02c459e7fe8798ff3452ad74f144a7fd05787f9f
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-09-21 07:42:50 -0400

    Update recom-patches-pictrack.js

--- recom-patches-pictrack.js
+++ recom-patches-pictrack.js
@@ -385,7 +385,15 @@ function pictureLogger_tableModal(date = null) {
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Picture Log</h5>
-                    <button type="button" class="btn-close" id="rc_ajax_modal_close" aria-label="Close"></button>
+                    <div class="btn btn-icon btn-sm btn-active-icon-primary btn-close" id="rc_ajax_modal_close" aria-label="Close">
+                        <span class="svg-icon svg-icon-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="currentColor"></rect>
+                                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="currentColor"></rect>
+                            </svg>
+                        </span>
+                    </div>
+
                 </div>
                 <div class="modal-body" id="rc_table_modal_content"></div>
                 <div class="modal-footer">
