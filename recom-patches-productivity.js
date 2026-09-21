d0871f8d901c2a62f284c9998cf826c7b142aa5d
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-09-21 12:29:41 -0400

    Update recom-patches-productivity.js

--- recom-patches-productivity.js
+++ recom-patches-productivity.js
@@ -579,16 +579,22 @@ async function injectUserReport() {
 
                 // get picture task units
                 let extraUnits = 0;
-                // get picture task units
                 const dateInput = document.getElementById('patches-productivity-dateInput');
                 if (task.toLowerCase() === 'pictures' && dateInput) {
                     const pictureStats = async () => {
                         // get person
-                        const person = user.trim().split(/\s+/)[0];
+                        const kt_header_user_menu_toggle = document.getElementById('kt_header_user_menu_toggle');
+                        if (!kt_header_user_menu_toggle) return;
+                        const nameElem = kt_header_user_menu_toggle.querySelector('.menu-sub > .menu-item > .menu-content > .d-flex.flex-column > .fw-bold.d-flex.align-items-center.fs-5');
+                        if (!nameElem) return
+                        const nameElemClone = nameElem.cloneNode(true);
+                        const badge = nameElemClone.querySelector('.badge');
+                        if (badge) badge.remove();
+                        const person = nameElemClone.textContent.trim();
 
                         // get data
                         const pictureTrackingData = await pictureLogger_fetch({person, date:dateInput.value});
-                        if (!pictureTrackingData || !pictureTrackingData.data) return {pictureCount:0, pictureItems:0};
+                        if (!pictureTrackingData || !pictureTrackingData.data) return;
 
                         // get sum of count
                         let count = 0;
@@ -597,7 +603,6 @@ async function injectUserReport() {
                         // return sum and item count
                         return {pictureCount:count, pictureItems:pictureTrackingData.data.length};
                     }
-
                     const {pictureCount, pictureItems} = await pictureStats() ?? {pictureCount: 0, pictureItems: 0};
                     totalUnits = pictureCount ?? 0;
                     extraUnits = pictureItems ?? 0;
@@ -613,13 +618,13 @@ async function injectUserReport() {
                 if (eventCode === task) { label = `${task}`; }
 
                 const unitBox = `
-                    <div class="card card-xl-stretch mb-xl-8" style="--bs-card-bg: rgb(65,40,50); color: white !important; flex: 1; min-width: 400px; margin-bottom: 0 !important;">
+                    <div class="card card-xl-stretch mb-xl-8" style="--bs-card-bg: rgb(65,40,50) !important; color: white !important; flex: 1; min-width: 400px;">
                         <div class="card-body d-flex flex-column">
                             <div class="d-flex flex-column flex-grow-1" style="margin-bottom: 1.5rem;">
-                                <span class="text-white fw-bolder fs-3">${task.toLowerCase() === 'pictures' ? 'Images Uplodaded (Items)' : 'Units Added'} | ${label}</span>
+                                <span class="text-white fw-bolder fs-3">Units Added | ${label}</span>
                             </div>
                             <div class="pt-5">
-                                <span class="text-white fw-bolder fs-3x me-2 lh-0">${totalUnits}${task.toLowerCase() === 'pictures' ? ` (${extraUnits})` : ''}</span>
+                                <span class="text-white fw-bolder fs-3x me-2 lh-0">${totalUnits}</span>
                                 <span class="text-white fw-bolder fs-6 lh-0">${timePerUnit} mins/unit</span>
                             </div>
                         </div>
