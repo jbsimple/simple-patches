68a243771a5890a1ea93258ea3977b6c044e87f7
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-09-21 12:32:25 -0400

    Update recom-patches-productivity.js

--- recom-patches-productivity.js
+++ recom-patches-productivity.js
@@ -634,7 +634,7 @@ async function injectUserReport() {
                 const timeBox = `
                     <div class="card card-xl-stretch mb-xl-8" style="--bs-card-bg: rgb(50,60,85) !important; color: white !important; flex: 1; min-width: 400px;">
                         <div class="card-body d-flex flex-column">
-                            <div class="d-flex flex-column flex-grow-1">
+                            <div class="d-flex flex-column flex-grow-1" style="margin-bottom: 1.5rem;">
                                 <span class="text-white fw-bolder fs-3">Time Spent | ${label}</span>
                             </div>
                             <div class="pt-5">
