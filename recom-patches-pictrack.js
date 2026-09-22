7006d509858c02d8fa103ac670e6eecdc8d134aa
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-09-22 06:38:34 -0400

    Update recom-patches-pictrack.js

--- recom-patches-pictrack.js
+++ recom-patches-pictrack.js
@@ -442,7 +442,7 @@ function pictureLogger_tableModal(date = null) {
     }
  
     modalEl.innerHTML = `
-        <style>#rc_table_modal_content table { max-height: 50vh !important; }</stule>
+        <style>#rc_table_modal_content table { max-height: 50vh !important; }</style>
         <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
