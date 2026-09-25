d6a8af85face4c4e9c5319c8c34bb7d3e5e19bbe
jbsimple <jonathan.bitterli@simplecellinc.com>
2026-09-25 08:09:09 -0400

    Update recom-patches-productivity.js

--- recom-patches-productivity.js
+++ recom-patches-productivity.js
@@ -831,9 +831,6 @@ async function injectTeamReport() {
                 Object.keys(userDataMap[user][task]).forEach(async eventCode => {
                     let { totalUnits, totalTime, clockIn, clockOut } = userDataMap[user][task][eventCode];
 
-                    console.debug('clockIn:', clockIn);
-                    console.debug('clockIn:', clockOut);
-
                     let units_label = 'Units Added';
                     let task_label = `"${eventCode}" while in ${task}`;
                     if (eventCode === task) { task_label = `${task}`; }
@@ -890,7 +887,7 @@ async function injectTeamReport() {
                         <div class="card card-xl-stretch" style="--bs-card-bg: rgb(50,60,85) !important; color: white !important; flex: 1; min-width: 400px; margin-bottom: 0 !important;">
                             <div class="card-body d-flex flex-column">
                                 <div class="d-flex flex-column flex-grow-1" style="margin-bottom: 1.5rem;">
-                                    <span class="text-white fw-bolder fs-3">Time Spent | ${label}</span>
+                                    <span class="text-white fw-bolder fs-3">Time Spent | ${task_label}</span>
                                 </div>
                                 <div class="pt-5">
                                     <span class="text-white fw-bolder fs-3x me-2 lh-0">${totalTime.toFixed(2)} min</span>
